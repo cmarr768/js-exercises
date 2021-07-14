75ea3c9444971b378cdf983f3ad4988dfdba0cc0
Colin Marr <colinm@ifit.com>
2021-07-14 10:29:00 -0400

    exercise 4

---
+++ src/exercise-04.test.js
@@ -0,0 +1,22 @@
+const user = require('../data/user.json');
+
+const omit = (object, fields) => {};
+
+describe('exercise-04', () => {
+  describe('omit', () => {
+    it('should return an object not containing the single field passed in', () => {
+      const { firstName, ...ans } = user;
+      expect(omit(user, 'firstName')).toStrictEqual(ans);
+    });
+
+    it('should return an object not containing the fields passed in', () => {
+      const { firstName, lastName, email, ...ans } = user;
+      expect(omit(user, ['firstName', 'lastName', 'email'])).toStrictEqual(ans);
+    });
+
+    it('should handle fields that do not exist', () => {
+      const { email, ...ans } = user;
+      expect(omit(user, ['email', 'address', 'updated'])).toStrictEqual(ans);
+    });
+  });
+});
