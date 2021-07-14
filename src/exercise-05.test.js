f3f98fbd464f5cab5220fb7b9a4c64c91619e098
Colin Marr <colinm@ifit.com>
2021-07-14 10:34:09 -0400

    exercise 5

---
+++ src/exercise-05.test.js
@@ -0,0 +1,27 @@
+const user = require('../data/user.json');
+
+const pick = (object, fields) => {};
+
+describe('exercise-05', () => {
+  describe('pick', () => {
+    it('should return an object only containing the single field passed in', () => {
+      expect(pick(user, 'firstName')).toStrictEqual({
+        firstName: user.firstName,
+      });
+    });
+
+    it('should return an object containing the fields passed in', () => {
+      expect(pick(user, ['firstName', 'lastName', 'email'])).toStrictEqual({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      });
+    });
+
+    it('should handle fields that do not exist', () => {
+      expect(pick(user, ['email', 'address', 'updated'])).toStrictEqual({
+        email: user.email,
+      });
+    });
+  });
+});
