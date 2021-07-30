70bc9d50abd811913f494bd9b5a59a35cab53c4c
Colin Marr <colinm@ifit.com>
2021-07-30 10:21:15 -0400

    exercise 8

---
+++ src/exercise-08.test.js
@@ -0,0 +1,31 @@
+const users = require('../data/users.json');
+
+const getSeries = (regions) => {
+  return regions.map((region) => {
+    switch (region) {
+      case 'kanto':
+        return 'indigo';
+      case 'kalos':
+        return 'xy';
+      default:
+        return;
+    }
+  });
+};
+
+const transformPokemon = (objects) => {};
+
+describe('exercise-07', () => {
+  describe('transformPokemon', () => {
+    beforeEach(() => {
+      jest.spyOn(Array.prototype, 'map');
+    });
+
+    it('should return each user with a series array', () => {
+      const response = transformPokemon(users);
+      expect(Array.prototype.map).toHaveBeenCalledTimes(6);
+      expect(response[0].series).toStrictEqual(['indigo', 'xy']);
+      expect(response[4].series).toStrictEqual(['xy']);
+    });
+  });
+});
